ae1aba674646c643f7e2125e3da39d8411b97a60
Mario Domgoergen <mdom@taz.de>
2016-02-15 21:51:25 +0100

    Assign default for timestamp and stringify

--- lib/App/twtxtpl/Tweet.pm
+++ lib/App/twtxtpl/Tweet.pm
@@ -6,17 +6,22 @@ use POSIX ();
 use Moo;
 
 has user      => ( is => 'ro' );
-has timestamp => ( is => 'ro', coerce => \&to_time_piece );
+has timestamp => ( is => 'ro', coerce => \&to_timestamp, default => sub { Mojo::Date->new() } );
 has text      => ( is => 'ro' );
 
-sub to_time_piece{
-	return Mojo::Date->new(shift);
+sub to_timestamp {
+    return ref $_[0] eq 'Mojo::Date' ? $_[0] : Mojo::Date->new( $_[0] );
 }
 
 sub strftime {
 	my ($self,$format ) = @_;
 	$DB::single=1;
 	return POSIX::strftime($format, gmtime shift->timestamp->epoch);
+}
+
+sub to_string {
+	my $self = shift;
+	return $self->strftime('%FT%T%z') . "\t" . $self->text;
 }
 
 1;
