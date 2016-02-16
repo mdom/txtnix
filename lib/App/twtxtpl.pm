0b8cc409fd54277bdb334139d0d9ea5db070a217
Mario Domgoergen <mdom@taz.de>
2016-02-16 21:45:11 +0100

    Add distribution code

--- lib/App/twtxtpl.pm
+++ lib/App/twtxtpl.pm
@@ -9,7 +9,10 @@ use Mojo::UserAgent;
 use Moo;
 use App::twtxtpl::Tweet;
 use IO::Pager;
+use String::ShellQuote;
 use File::Basename qw(basename);
+
+our $VERSION = '0.01';
 
 has name => ( is => 'ro', default => sub { basename $0 } );
 has config => ( is => 'lazy' );
@@ -134,3 +137,27 @@ sub following {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+twtxtpl - Decentralised, minimalist microblogging service for hackers
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015 Mario Domgoergen C<< <mario@domgoergen.com> >>
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
