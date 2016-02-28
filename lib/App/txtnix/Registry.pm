7e53383b5e29f89575f46c6e20e59bf806714fba
Mario Domgoergen <mdom@taz.de>
2016-02-28 21:23:41 +0100

    Allow non blocking access to registry

--- lib/App/txtnix/Registry.pm
+++ lib/App/txtnix/Registry.pm
@@ -1,54 +1,59 @@
 package App::txtnix::Registry;
 use Mojo::Base -base;
 use Mojo::UserAgent;
+use Mojo::URL;
+use Mojo::ByteStream 'b';
 use Carp;
 
-has 'url';
-has ua => sub {
-    return Mojo::UserAgent->new();
-};
+has [ 'url', 'ua' ];
 
 sub get_users {
-    my ( $self, $user ) = @_;
-    my $query = Mojo::URl->new( $self->url )->path('/api/plain/users')
+    my ( $self, $user, $cb ) = @_;
+    my $query = Mojo::URL->new( $self->url )->path('/api/plain/users')
       ->query( q => $user || '' );
-    return $self->query_endpoint($query);
+    return $self->query_endpoint( $query, $cb );
 }
 
 sub get_tweets {
-    my ( $self, $text ) = @_;
-    my $query = Mojo::URl->new( $self->url )->path('/api/plain/tweets')
+    my ( $self, $text, $cb ) = @_;
+    my $query = Mojo::URL->new( $self->url )->path('/api/plain/tweets')
       ->query( q => $text || '' );
-    return $self->query_endpoint($query);
+    return $self->query_endpoint( $query, $cb );
 }
 
 sub get_tag {
-    my ( $self, $tag ) = @_;
+    my ( $self, $tag, $cb ) = @_;
     croak('Parameter tag must be provided for get_tag.')
       if not $tag;
-    my $query = Mojo::URl->new( $self->url )->path("/api/plain/tags/$tag");
-    return $self->query_endpoint($query);
+    my $query = Mojo::URL->new( $self->url )->path("/api/plain/tags/$tag");
+    return $self->query_endpoint( $query, $cb );
 }
 
 sub get_mentions {
-    my ( $self, $url ) = @_;
+    my ( $self, $url, $cb ) = @_;
     croak('Parameter url must be provided for get_mentions.')
       if not $url;
-    my $query = Mojo::URl->new( $self->url )->path('/api/plain/mentions')
+    my $query = Mojo::URL->new( $self->url )->path('/api/plain/mentions')
       ->query( url => $url );
-    return $self->query_endpoint($query);
+    return $self->query_endpoint( $query, $cb );
 }
 
 sub query_endpoint {
-    my ( $self, $endpoint ) = @_;
-    my $tx = $self->ua->get($endpoint);
-    my @result;
-    if ( my $res = $tx->success ) {
-        for my $line ( split /\n/, $res->body ) {
-            push @result, [ split /\t/, $line ];
+    my ( $self, $endpoint, $cb ) = @_;
+    croak('Missing parameter callback.') if not $cb;
+    $self->ua->get(
+        $endpoint => sub {
+            my ( $ua, $tx ) = @_;
+            my @result;
+            if ( my $res = $tx->success ) {
+                for my $line ( split /\n/, b( $res->body )->decode ) {
+                    push @result, [ split /\t/, $line ];
+                }
+            }
+            $cb->(@result);
         }
-    }
-    return @result;
+    );
+    return;
 }
 
 1;
