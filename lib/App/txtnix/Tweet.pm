3af86f95ad50fe9a2153503c6948052111476075
Mario Domgoergen <mdom@taz.de>
2016-02-28 21:19:40 +0100

    Compute md5 hash of tweet

--- lib/App/txtnix/Tweet.pm
+++ lib/App/txtnix/Tweet.pm
@@ -1,6 +1,7 @@
 package App::txtnix::Tweet;
 use Mojo::Base -base;
 use HTTP::Date 'str2time';
+use Mojo::ByteStream 'b';
 use POSIX ();
 
 has [ 'user', 'text' ];
@@ -14,6 +15,11 @@ sub strftime {
 sub to_string {
     my $self = shift;
     return $self->strftime('%FT%T%z') . "\t" . $self->text;
+}
+
+sub md5_hash {
+    my $self = shift;
+    return b( $self->timestamp . $self->user . $self->text )->encode->md5_sum;
 }
 
 1;
