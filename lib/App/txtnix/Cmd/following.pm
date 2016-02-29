f7a10be304711e16598f3075e543ab1a36b5a5e2
Mario Domgoergen <mdom@taz.de>
2016-02-29 20:58:41 +0100

    Remove unneeded code

--- lib/App/txtnix/Cmd/following.pm
+++ lib/App/txtnix/Cmd/following.pm
@@ -1,11 +1,9 @@
 package App::txtnix::Cmd::following;
 use Mojo::Base 'App::txtnix';
 
-has 'nick';
-has 'url';
 
 sub run {
-    my ( $self, $whom, $url ) = @_;
+    my ($self)    = @_;
     my %following = %{ $self->following };
     for my $user ( keys %following ) {
         print "$user \@ " . $following{$user} . "\n";
