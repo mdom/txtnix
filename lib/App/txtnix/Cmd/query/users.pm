f54b31a1e59e6ddb0c1ffb0cddee98864f060112
Mario Domgoergen <mdom@taz.de>
2016-03-03 21:50:41 +0100

    Use the new blocking interface to query_endpoint

--- lib/App/txtnix/Cmd/query/users.pm
+++ lib/App/txtnix/Cmd/query/users.pm
@@ -11,23 +11,17 @@ sub run {
     my $registry =
       App::txtnix::Registry->new( url => $self->registry, ua => $self->ua );
 
-    my $delay = Mojo::IOLoop->delay;
-    my $end   = $delay->begin;
-    $registry->get_users(
-        $self->search_term,
-        sub {
-            my @results = @_;
-            if ( $self->limit && @results > $self->limit ) {
-                @results = @results[ 0 .. $self->limit - 1 ];
-            }
-            for my $result (@results) {
-                my ( $url, undef, $nick ) = @$result;
-                print "$nick \@ $url\n";
-            }
-            $end->();
-        }
-    );
-    $delay->wait;
+    my @results = $registry->get_users( $self->search_term );
+
+    if ( $self->limit && @results > $self->limit ) {
+        @results = @results[ 0 .. $self->limit - 1 ];
+    }
+
+    for my $result (@results) {
+        my ( $url, undef, $nick ) = @$result;
+        print "$nick @ $url\n";
+    }
+
     return 0;
 }
 
