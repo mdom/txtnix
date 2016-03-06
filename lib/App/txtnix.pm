79b3b6d9112ef7209e21900195fc40b0ff8cf140
Mario Domgoergen <mdom@taz.de>
2016-03-06 22:26:18 +0100

    0.06

--- lib/App/txtnix.pm
+++ lib/App/txtnix.pm
@@ -14,7 +14,7 @@ use App::txtnix::Registry;
 use IO::Pager;
 use Mojo::ByteStream 'b';
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 has ua      => sub { shift->_build_ua };
 has cache   => sub { App::txtnix::Cache->new( cache_dir => shift->cache_dir ) };
