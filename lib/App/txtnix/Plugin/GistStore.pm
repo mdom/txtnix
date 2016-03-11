636a697dc98f3339365fc7680dd9067f27a3a6d1
Mario Domgoergen <mdom@taz.de>
2016-03-11 20:27:08 +0100

    Add Plugin GistStore to upload twtxt to gist

---
+++ lib/App/txtnix/Plugin/GistStore.pm
@@ -0,0 +1,65 @@
+package App::txtnix::Plugin::GistStore;
+use strict;
+use warnings;
+use Mojo::Base 'App::txtnix::Plugin';
+use Path::Tiny;
+use Mojo::JSON qw(true false);
+
+has handlers => sub {
+    { post_tweet => \&store };
+};
+
+sub store {
+    my $app = shift;
+    my $ua  = $app->ua;
+
+    my $config = $app->read_config;
+
+    my $store_config = $config->{'GistStore'};
+
+    return if !$store_config;
+
+    my $token    = $store_config->{access_token};
+    my $username = $store_config->{user};
+    my $id       = $store_config->{id};
+
+    my $url =
+      Mojo::URL->new("https://api.github.com/gists")
+      ->userinfo("$username:$token");
+
+    if ($id) {
+        $url->path( $url->path . '/' . $id );
+    }
+
+    my $file = $app->twtfile;
+
+    my $tx = $ua->post(
+        $url => json => {
+            description => "twtxt.txt for $username",
+            public      => true,
+            files       => {
+                "twtxt.txt" => {
+                    content => $file->slurp_utf8,
+                }
+            }
+        }
+    );
+
+    if ( my $res = $tx->success ) {
+        print "Uploaded gist.\n";
+        if ( !$id ) {
+            $config->{'Store::Gist'}->{id} = $res->json->{id};
+            $config->write( $app->config, 'utf8' );
+        }
+    }
+    else {
+        my $err   = $tx->error;
+        my $error = $err->{message};
+        $error = $err->code . " $error" if $err->{code};
+        warn "Error while uploading gist: $error\n";
+    }
+
+    return;
+}
+
+1;
