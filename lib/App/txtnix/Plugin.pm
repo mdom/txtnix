341439f165e252559cde84a04370a336e32f288d
Mario Domgoergen <mdom@taz.de>
2016-03-12 22:39:49 +0100

    Return empty hash as plugin config default

--- lib/App/txtnix/Plugin.pm
+++ lib/App/txtnix/Plugin.pm
@@ -10,7 +10,7 @@ has config   => sub { shift->_build_config };
 sub _build_config {
     my $self = shift;
     my ($name) = ref($self) =~ /^.*::(.*)$/;
-    return $self->app->read_config->{$name};
+    return $self->app->read_config->{$name} || {};
 }
 
 sub is_enabled {
