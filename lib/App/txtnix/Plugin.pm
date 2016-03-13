c2ff1bdecdc630a2cf1333ba469f42b17bdfc927
Mario Domgoergen <mdom@taz.de>
2016-03-13 14:11:25 +0100

    Enable ShellExec by default

--- lib/App/txtnix/Plugin.pm
+++ lib/App/txtnix/Plugin.pm
@@ -6,6 +6,7 @@ use Mojo::Base -base;
 has 'app';
 has handlers => sub { {} };
 has config   => sub { shift->_build_config };
+has name     => sub { my ($name) = ref( $_[0] ) =~ /^.*::(.*)$/; return $name };
 
 sub _build_config {
     my $self = shift;
@@ -16,6 +17,8 @@ sub _build_config {
 sub is_enabled {
     my $self   = shift;
     my $config = $self->config;
+
+    return 1 if $self->name eq 'ShellExec';
     return defined $self->config && $self->config->{enabled};
 }
 
