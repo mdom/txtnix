b0ab2c2f8cb9a397487b155ca20e58bbda3bf9f2
Mario Domgoergen <mdom@taz.de>
2016-08-03 23:27:42 +0200

    Use autopopulated attributes in FTP.pm

--- lib/App/txtnix/Plugin/FTP.pm
+++ lib/App/txtnix/Plugin/FTP.pm
@@ -3,45 +3,39 @@ use Mojo::Base 'App::txtnix::Plugin';
 use Net::FTP;
 
 has ssl => sub { 1 };
+has [qw(host user password remote_file)];
 
 sub post_tweet {
     my $self = shift;
-    my $app  = $self->app;
-    my $ua   = $app->ua;
 
-    my $host        = $self->config->{host};
-    my $user        = $self->config->{user};
-    my $password    = $self->config->{password};
-    my $remote_file = $self->config->{remote_file};
-
-    my $file = $app->twtfile;
+    my $file = $self->app->twtfile;
 
     if ( !$file || !$file->exists ) {
         warn "Can't find twtfile to upload\n";
         return;
     }
 
-    if ( !$host ) {
+    if ( !$self->host ) {
         warn "Missing host for ftp upload\n";
         return;
     }
 
-    my $ftp = Net::FTP->new( $host, SSL => $self->ssl );
+    my $ftp = Net::FTP->new( $self->host, SSL => $self->ssl );
 
     if ( !$ftp ) {
-        warn "Cannot connect to ftp://$host: $@\n";
+        warn "Cannot connect to ftp://", $self->host, ": $@\n";
         return;
     }
 
-    if ( !$ftp->login( $user, $password ) ) {
-        warn "Cannot login to ftp server ", $ftp->message . "\n";
+    if ( !$ftp->login( $self->user, $self->password ) ) {
+        warn "Cannot login to ftp server ", $ftp->message, "\n";
         return;
     }
 
     $ftp->binary;
 
-    if ( !$ftp->put( $file->stringify, $remote_file ) ) {
-        warn "Cannot upload $file: " . $ftp->message . "\n";
+    if ( !$ftp->put( $file->stringify, $self->remote_file ) ) {
+        warn "Cannot upload $file: ", $ftp->message, "\n";
         return;
     }
 
